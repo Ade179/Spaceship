56ac5d2aa2c699e93023ebfb458bb8524e8b3aae
Juwon <dnladesanya1@gmail.com>
2022-11-01 11:55:19 +0100

    set up footer header and populate page with imgs from API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
+import display from './modules/display.js';
+import getTheApi from './modules/getTheApi.js';
+
+display();
+getTheApi();
