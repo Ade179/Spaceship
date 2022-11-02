e1d6e8c76e51e36359c70e051aae8429b4ae2857
Bludidi <bludidi5@gmail.com>
2022-11-02 19:01:07 +0200

    add style to the modal

--- src/modules/commentsModal.js
+++ src/modules/commentsModal.js
@@ -9,7 +9,7 @@ const popup = (id, rockets) => {
 <div class="content">
     <h1>${rocket.name}</h1>
     <div class="info">
-        <ul class="line1">
+       <div class='rocket-info'> <ul class="line1">
             <li><span>Logistics Service Provider:</span> ${rocket.lsp_name}</li>
             <li><span>Orbit:</span> ${rocket.orbit}</li>
             <li><span>Pad:</span> ${rocket.pad}</li>
@@ -22,13 +22,15 @@ const popup = (id, rockets) => {
     </div>
     <h3>comment(2)</h3>
     <ul class ="comments-section">
+    <li>01/11/2022 Chris: It's beautiful</li>
+    <li>02/11/2022 Hazel: I really like this one!</li>
     </ul>
-
-    <h3>Add comment</h3>
+    </div>
     <form>
-        <input type="text" class="input" placeholder="your name">
-        <textarea name="insight" class="input" cols="30" rows="10" placeholder="your insights"></textarea>
-        <button type="button">comment</button>
+    <h3 id='com-head'>Add comment</h3>
+        <input type="text" class="input" id="name" placeholder="your name">
+        <textarea name="insight" class="input" id="insight" cols="30" rows="10" placeholder="your insights"></textarea>
+        <button type="button" class='com-btn'>Comment</button>
     </form>
 </div>    
     `;
