30493fcd6202e1f940634b0e131584474008247f
Juwon <dnladesanya1@gmail.com>
2022-11-02 15:22:59 +0100

    populate html

--- src/modules/commentsModal.js
+++ src/modules/commentsModal.js
@@ -1,29 +1,43 @@
-const commentModal = document.querySelector('.com-mod');
-const commentBtn = document.querySelectorAll('.com-btn');
-const exit = document.querySelector('.close');
+const commentModal = document.querySelector('.modalcomments');
+const popup = (id, rockets) => {
+  const rocket = rockets.find((data) => data.id === id);
+  commentModal.innerHTML = `
+    <div class="titles">
+    <img class="rocketimage" src="${rocket.image}" alt="rocket">
+    <button class="close">&times;</button>
+</div>
+<div class="content">
+    <h1>${rocket.name}</h1>
+    <div class="info">
+        <ul class="line1">
+            <li><span>Logistics Service Provider:</span> ${rocket.lsp_name}</li>
+            <li><span>Orbit:</span> ${rocket.orbit}</li>
+            <li><span>Pad:</span> ${rocket.pad}</li>
+        </ul>
+        <ul class="line2">
+        <li><span>Mission:</span> ${rocket.mission}</li>
+        <li><span>Mission Type:</span> ${rocket.mission_type}</li>
+        <li><span>Location:</span> ${rocket.location}</li>
+        </ul>
+    </div>
+    <h3>comment(2)</h3>
+    <ul class ="comments-section">
+    </ul>
 
-const exitBtn = document.createElement('div');
-exitBtn.classList.add('exit');
-exitBtn.innerHTML = `
-     <button class="close">&#10006
- </button>
-     `;
-commentModal.append(exitBtn);
+    <h3>Add comment</h3>
+    <form>
+        <input type="text" class="input" placeholder="your name">
+        <textarea name="insight" class="input" cols="30" rows="10" placeholder="your insights"></textarea>
+        <button type="button">comment</button>
+    </form>
+</div>    
+    `;
 
-const showModal = () => {
-  commentModal.style.display = 'block';
-  commentModal.classList.toggle('open');
-  exit.style.visibility = 'visible';
+  commentModal.classList.toggle('active');
+  const close = document.querySelector('.close');
+  close.addEventListener('click', () => {
+    commentModal.classList.remove('active');
+  });
 };
 
-const closeModal = () => {
-  commentModal.style.display = 'none';
-  commentModal.classList.remove('open');
-  exit.style.visibility = 'hidden';
-};
-
-commentBtn.addEventListener('click', showModal);
-exit.addEventListener('click', closeModal);
-
-exports.showModal = showModal;
-exports.closeModal = closeModal;
+module.exports = popup;
--- src/modules/getTheApi.js
+++ src/modules/getTheApi.js
@@ -1,4 +1,5 @@
 import display from './display.js';
+import popup from './commentsModal.js';
 
 const baseUrl = 'https://ll.thespacedevs.com/2.2.0/launch/upcoming/?mode=list&status=8&ordering=window_end';
 const counter = document.querySelector('.count');
@@ -6,6 +7,12 @@ const getTheApi = async () => {
   const data = await fetch(baseUrl);
   const res = await data.json();
   display(res.results);
+  document.addEventListener('click', (e) => {
+    const { id } = e.target;
+    if (e.target.matches('.comment')) {
+      popup(id, res.results);
+    }
+  });
   counter.innerText = res.count;
 };
 
