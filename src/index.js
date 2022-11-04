0f6453eab51a3e1a6f9d56a053200a959f38a042
Bludidi <bludidi5@gmail.com>
2022-11-04 16:18:55 +0200

    fix lint error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css';
-// import '@fortawesome/fontawesome-free/css/all.css';
-// import '@fortawesome/fontawesome-free/js/all.js';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
 import getTheApi from './modules/getTheApi.js';
 import render from './modules/render.js';
 
